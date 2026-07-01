8a34da99f15d9555024e3e952a3b626864087e5c
Kunal V Varal <kunalvvaral@gmail.com>
2026-07-01 11:49:47 +0530

    Day 2: Added if-else, switch and half pyramid pattern solutions

---
+++ Day00_Loops.java/ifElse.java
@@ -0,0 +1,61 @@
+// import java.util.*;
+
+// public class halfPyramidPattern {
+//     public static void main(String args[]){
+//         int a = 4;
+//         int b = 3;
+//         if(a >= b){
+//             System.out.println("A");
+//         }else{
+//             System.out.println("B");
+//         }
+//     }
+// }
+
+// public class halfPyramidPattern {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         if(num % 2 == 0){
+//             System.out.println("Num is Even");
+//         }else{
+//             System.out.println("Num is Odd");
+//         }
+//     }
+// }
+
+
+// public class halfPyramidPattern {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int income = sc.nextInt();
+//         int tax = 0;
+//         if(income <= 500000){
+//         }else if(income > 500000 && income < 1000000 ){
+//             tax = (int)(income * (0.2));
+//         }else {
+//             tax = (int)(income * (0.3));
+//         }
+//             System.out.println("tax is : " + tax);
+//     }
+// }
+
+// public class halfPyramidPattern {
+//     public static void main (String args[]){
+//         int a = 1;
+//         int b = 3;
+//         int c = 6;
+
+//       String type = (a >= b && a >= c)? "A is largest":((b >= c)?"B is Largest":"C is largest");
+//       System.out.println(type);
+//     }
+// }
+
+// public class halfPyramidPattern {
+//     public static void main (String args[]){
+//         int marks = 4;
+
+//         String type = (marks >= 33)?"pass":"fail";
+//         System.out.println(type);
+//     }
+// }
---
+++ Day00_Loops.java/switchStatement.java
@@ -0,0 +1,43 @@
+// import java.util.*;
+
+
+// public class halfPyramidPattern {
+//     public static void main (String args[]) {
+//         char ch = 'a';
+//        int number = 2;
+//        switch(ch){
+//         case 'a' : System.out.println("Sam");
+//         break;
+//         case 3 : System.out.println("Ram");
+//         break;
+//         case 2 : System.out.println("Lam");
+//         break;
+//         default : System.out.println("None");
+//        }
+//     }
+// }
+
+// public class halfPyramidPattern {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the Num a : ");
+//         int a = sc.nextInt();
+//         System.out.println("Enter the Num b : ");
+//         int b = sc.nextInt();
+//         System.out.println("Enter the operator : ");
+//         char operator = sc.next().charAt(0);
+
+//         switch(operator) {
+//             case '+' : System.out.println(a + b);
+//             break;
+//             case '-' : System.out.println(a - b);
+//             break;
+//             case '*' : System.out.println(a * b);
+//             break;
+//             case '/' : System.out.println(a / b);
+//             break;
+//             case '%' : System.out.println(a % b);
+//             break;
+//             default : System.out.println("The number is not valid");
+// }}
+// }
