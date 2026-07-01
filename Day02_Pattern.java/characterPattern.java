4571b483983edf286502e49e116642aeac8130a2
Kunal V Varal <kunalvvaral@gmail.com>
2026-07-01 16:23:33 +0530

    Added Half-pyramid-pattern and Character-pattern for Day 2

---
+++ Day02_Pattern.java/characterPattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class characterPattern{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        char ch = 'A';
+        for(int i = 1; i<= num;i++){
+            for(int j = 1; j <= i;j++){
+                System.out.print(ch + " ");
+                ch++;
+            }
+                System.out.println( );
+        } };
+};
