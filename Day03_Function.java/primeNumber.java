43b7511070ed9c683fddce8e6b9fb83fdcd9366f
Kunal V Varal <kunalvvaral@gmail.com>
2026-07-06 14:20:39 +0530

    Added one Question with optamized

--- Day03_Function.java/primeNumber.java
+++ Day03_Function.java/primeNumber.java
@@ -2,9 +2,19 @@ import java.util.*;
 
 public class primeNumber {
 
-   public static int 
+   public static boolean isPrime(int n){
+    boolean isPrime =true;
+    for(int i =2 ; i <= n -1;i++){
+        if(n %  i ==0){   // Completely dividing 
+             isPrime = false;
+            //  break;  // can use it but driectly return
+            // return isPrime;
+        }
+    }
+    return isPrime;
+   } 
 
     public static void main(String args[]){
-        
+          System.out.println(isPrime(13));
     }
 }
